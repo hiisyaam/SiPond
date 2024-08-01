101ba08ee2a608f596cfe043f072c55b792271b7
Abigail <farishiisyamhardimann@gmail.com>
2024-08-01 22:08:20 +0700

    penamaan title

--- app/Controllers/Absen.php
+++ app/Controllers/Absen.php
@@ -4,14 +4,14 @@ namespace App\Controllers;
 
 class Absen extends BaseController
 {
-    public function index():string
-    {
-        helper('date');
-        $data = [
-            'tanggal_sekarang' => format_hari_tanggal(date('Y-m-d H:i:s'))
-        ];
-        return view('dashboard',$data);
-    }
+    // public function index():string
+    // {
+    //     helper('date');
+    //     $data = [
+    //         'tanggal_sekarang' => format_hari_tanggal(date('Y-m-d H:i:s'))
+    //     ];
+    //     return view('dashboard',$data);
+    // }
 
     public function SD():string
     {
@@ -19,7 +19,7 @@ class Absen extends BaseController
         $data = [
             'title' => 'Absensi SD',
             'tanggal_sekarang' => format_hari_tanggal(date('Y-m-d H:i:s')),  
-            'unit' => 'SD'         
+            'unit' => 'Unit SD'         
         ];
         return view('absensi',$data);
     }
@@ -28,17 +28,32 @@ class Absen extends BaseController
     {
         helper('date');
         $data = [
-            'tanggal_sekarang' => format_hari_tanggal(date('Y-m-d H:i:s'))
+            'title' => 'Absensi SMP',
+            'tanggal_sekarang' => format_hari_tanggal(date('Y-m-d H:i:s')),  
+            'unit' => 'Unit SMP'         
         ];
-        return view('dashboard',$data);
+        return view('absensi',$data);
     }
 
     public function SMA():string
     {
         helper('date');
         $data = [
-            'tanggal_sekarang' => format_hari_tanggal(date('Y-m-d H:i:s'))
+            'title' => 'Absensi SMA',
+            'tanggal_sekarang' => format_hari_tanggal(date('Y-m-d H:i:s')),  
+            'unit' => 'Unit SMA'         
         ];
-        return view('dashboard',$data);
+        return view('absensi',$data);
+    }
+
+    public function karyawan():string
+    {
+        helper('date');
+        $data = [
+            'title' => 'Absensi Karyawan',
+            'tanggal_sekarang' => format_hari_tanggal(date('Y-m-d H:i:s')),  
+            'unit' => 'Karyawan'         
+        ];
+        return view('absensi',$data);
     }
 }
